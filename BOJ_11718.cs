1aae69098cab6f74e828e722159dd1f56de64747
name <swlee1112@naver.com>
2023-08-16 21:04:07 +0900

    Create BOJ_11718.cs

---
+++ BOJ_11718.cs
@@ -0,0 +1,13 @@
+﻿// BOJ_11718
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input == null ) 
+        break;
+    sb.AppendLine($"{input}");
+}
+Console.WriteLine(  sb);
