49ff6890957b62b40138fd7c12b068154c8afd04
name <swlee1112@naver.com>
2023-08-16 21:03:36 +0900

    Create BOJ_10872.cs

---
+++ BOJ_10872.cs
@@ -0,0 +1,15 @@
+﻿// BOJ_10872
+
+
+int n = int.Parse(Console.ReadLine());
+int ans = 1;
+if(n == 0)
+{
+    Console.WriteLine(  ans);
+    return;
+}
+
+for (int i = 1; i <= n; i++)
+    ans *= i;
+Console.WriteLine(  ans);
+
