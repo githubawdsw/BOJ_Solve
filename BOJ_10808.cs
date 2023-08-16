1451e7b894151026d493d76d9a985b017878bf9b
name <swlee1112@naver.com>
2023-08-16 21:03:28 +0900

    Create BOJ_10808.cs

---
+++ BOJ_10808.cs
@@ -0,0 +1,16 @@
+﻿
+static StreamReader sr = new StreamReader(Console.OpenStandardInput());
+string s = sr.ReadLine();
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+Dictionary<char, int> dic = new Dictionary<char, int>();
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < alphabet.Length; i++)
+dic.Add(alphabet[i], 0);
+for (int i = 0; i < s.Length; i++)
+dic[s[i]]++;
+
+foreach (var one in dic)
+sb.Append( $"{  one.Value} " );
+Console.Write(sb);
