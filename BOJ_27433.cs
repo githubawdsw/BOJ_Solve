7cd5a286010ed0b76589fae08d34dfd180817076
name <swlee1112@naver.com>
2023-08-16 21:04:35 +0900

    Create BOJ_27433.cs

---
+++ BOJ_27433.cs
@@ -0,0 +1,15 @@
+﻿//BOJ_27433
+
+
+int n = int.Parse(Console.ReadLine());
+
+if (n == 0)
+{
+    Console.WriteLine(  "1");
+    return;
+}
+
+long ans = 1;
+for (int i = 1; i <= n; i++)
+    ans *= i;
+Console.WriteLine(  ans);
