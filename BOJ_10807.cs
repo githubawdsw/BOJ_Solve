cbc1e925bbaf455d9381637c02df20ec2f0fb153
name <swlee1112@naver.com>
2023-08-16 21:02:06 +0900

    Create BOJ_10807.cs

---
+++ BOJ_10807.cs
@@ -0,0 +1,10 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+string[] inputarr = Console.ReadLine().Split(' ');
+int v = int.Parse(Console.ReadLine());
+
+int[] counting = new int[201];
+for (int i = 0; i < n; i++)
+    counting[int.Parse(inputarr[i]) + 100]++;
+
+Console.WriteLine(counting[v +100]);
