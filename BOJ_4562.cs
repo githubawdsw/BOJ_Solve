d9b0ff47196672902e0b1ba278ea3a8618ff4a0a
name <swlee1112@naver.com>
2023-08-17 15:52:03 +0900

    Create BOJ_4562.cs

---
+++ BOJ_4562.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text;
+
+
+int n = int.Parse(Console.ReadLine());
+StringBuilder sb = new StringBuilder();
+for (int i = 0; i < n; i++)
+{
+    string[] inputer = Console.ReadLine().Split(' ');
+    int eat = int.Parse(inputer[0]);
+    int require = int.Parse(inputer[1]);
+    if (eat >= require)
+        sb.AppendLine("MMM BRAINS");
+    else
+        sb.AppendLine("NO BRAINS");
+}
+Console.WriteLine(sb);
