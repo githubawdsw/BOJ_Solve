d66a655f4d3a31da049c0034bd023ebbe6ed7e69
name <swlee1112@naver.com>
2023-08-17 15:52:01 +0900

    Create BOJ_25304.cs

---
+++ BOJ_25304.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Baekjoon1
+{
+    
+    class BOJ_25304
+    {
+        static void Main(string[] args)
+        {
+            long x = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            long sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputWorth = Console.ReadLine().Split(' ');
+                sum += long.Parse(inputWorth[0]) * long.Parse(inputWorth[1]);
+            }
+            if(sum == x)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+
+        }
+    }
+    
+}
