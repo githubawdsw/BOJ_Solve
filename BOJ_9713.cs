0a9b86fb2e98bcd1d5f54d9f4a2563d76f19e35f
name <swlee1112@naver.com>
2023-08-18 20:54:24 +0900

    Create BOJ_9713.cs

---
+++ BOJ_9713.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+int t = int.Parse(Console.ReadLine());
+while (t-- >0)
+{
+    int n = int.Parse(Console.ReadLine());
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 1)
+            sum += i;
+    }
+    sb.AppendLine(sum.ToString());
+}
+Console.WriteLine(sb);
