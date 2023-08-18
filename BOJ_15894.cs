d3b85cdbef9a3eb52d583a4349603990af71efea
name <swlee1112@naver.com>
2023-08-18 20:53:24 +0900

    Create BOJ_15894.cs

---
+++ BOJ_15894.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MathAlgorithm
+{
+    
+    class BOJ_15894
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(n *4);
+        }
+    }
+    
+}
