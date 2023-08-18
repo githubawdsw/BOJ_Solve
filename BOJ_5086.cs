f3022918217672aad965a9f90eca2d5135c6469d
name <swlee1112@naver.com>
2023-08-18 20:54:20 +0900

    Create BOJ_5086.cs

---
+++ BOJ_5086.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_5086
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int n = int.Parse(input[0]);
+                int m = int.Parse(input[1]);
+                if (n == 0 && m == 0)
+                    break;
+
+                if (n > m)
+                {
+                    if (n % m == 0)
+                        sb.AppendLine("multiple");
+                    else
+                        sb.AppendLine("neither");
+                }
+                else
+                {
+                    if (m % n == 0)
+                        sb.AppendLine("factor");
+                    else
+                        sb.AppendLine("neither");
+                }
+            }
+                Console.WriteLine(  sb);
+        } 
+    }
+    
+}
