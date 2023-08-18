c51d25aad5634a71c798654c22e9309c4b9d457a
name <swlee1112@naver.com>
2023-08-18 20:53:27 +0900

    Create BOJ_4892.cs

---
+++ BOJ_4892.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+int n;
+int count = 1;
+while (true)
+{
+    n = int.Parse(Console.ReadLine());
+    if (n == 0)
+        break;
+    sb.Append($"{count++}. ");
+    int find = n / 2;
+    if (n % 2 == 0)
+        sb.AppendLine($"even {find}");
+    else
+        sb.AppendLine($"odd {find}");
+                
+}
+Console.WriteLine(sb);
+
