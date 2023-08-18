079b3e9f8d473e4818999a797ea76ceb598f1168
name <swlee1112@naver.com>
2023-08-18 20:53:17 +0900

    Create BOJ_10162.cs

---
+++ BOJ_10162.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace GreedyAlgorithm
+{
+    
+    class BOJ_10162
+    {
+        static int t;
+        static void Main(string[] args)
+        {
+            t = int.Parse(Console.ReadLine());
+
+            int a = 300;
+            int b = 60;
+            int c = 10;
+            StringBuilder sb = new StringBuilder();
+            
+            sb.Append($"{t/a} ");
+            t = t % a;
+            
+            sb.Append($"{t / b} ");
+            t = t % b;
+            
+            sb.Append($"{t / c} ");
+            t = t % c;
+            
+            
+            if(t > 0)
+                Console.WriteLine(  -1);
+            else
+                Console.WriteLine(  sb);
+        }
+    }
+    
+}
