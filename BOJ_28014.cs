e0125bafa52d6a9d693634c858c20973146fd429
name <swlee1112@naver.com>
2023-08-19 16:52:56 +0900

    Create BOJ_28014.cs

---
+++ BOJ_28014.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Baekjoon1
+{
+    
+    class BOJ_28014
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] inputArr = Console.ReadLine().Split(' ');
+            int count = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (int.Parse(inputArr[i - 1]) <= int.Parse(inputArr[i]))
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+    
+}
