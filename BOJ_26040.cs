4f2eb1e04a0035f6875398d5cc5867a6668d12b5
name <swlee1112@naver.com>
2023-08-19 16:52:55 +0900

    Create BOJ_26040.cs

---
+++ BOJ_26040.cs
@@ -0,0 +1,31 @@
+﻿ // BOJ_26040
+
+using System.Text;
+    
+        
+string inputValue = Console.ReadLine();
+string[] ConvertWord = Console.ReadLine().Split(" ");
+string lowerWord = "";
+foreach (var one in ConvertWord)
+{
+    lowerWord += one.ToLower();
+}
+StringBuilder sb = new StringBuilder();
+int count = 0;
+for (int i = 0; i < inputValue.Length; i++)
+{
+    count = 0;
+    while (count < ConvertWord.Length)
+    {
+        if (inputValue[i] == char.Parse(ConvertWord[count]))
+        {
+            sb.Append( lowerWord[count]);
+            break;
+        }
+        count++;
+    }
+    if (count == ConvertWord.Length)
+        sb.Append(inputValue[i]);
+}
+Console.WriteLine(sb);
+        
