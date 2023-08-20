f7568bb26d3d7635b989b889d5923d13f7963ea4
name <swlee1112@naver.com>
2023-08-20 16:53:46 +0900

    Create BOJ_1919.cs

---
+++ BOJ_1919.cs
@@ -0,0 +1,18 @@
+﻿// BOJ_1919
+
+
+string one = Console.ReadLine();
+string two = Console.ReadLine();
+int[] onearr = new int[30];
+int[] twoarr = new int[30];
+
+foreach (var i in one)
+    onearr[i - 'a']++;
+foreach (var i in two)
+    twoarr[i - 'a']++;
+
+int ans = 0;
+for (int i = 0; i < 26; i++)
+    ans += Math.Abs(onearr[i] - twoarr[i]);
+
+Console.WriteLine(ans);
