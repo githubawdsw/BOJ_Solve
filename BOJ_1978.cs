670b35f3bf2a61ec1801a57de36e5eca9defcf86
name <swlee1112@naver.com>
2023-08-20 16:53:48 +0900

    Create BOJ_1978.cs

---
+++ BOJ_1978.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_1978
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] inputarr = Console.ReadLine().Split(' ');
+
+            int ans = 0;
+            for (int i = 0; i < inputarr.Length; i++)
+            {
+                if (int.Parse(inputarr[i]) == 1)
+                    continue;
+                bool check = false;
+                for (int j = 2; j*j < int.Parse(inputarr[i]); j++)
+                {
+                    if (int.Parse(inputarr[i]) % j == 0)
+                        check = true;
+                }
+                if (!check)
+                    ans++;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+    
+}
