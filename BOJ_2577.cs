799487cc2c4ba2cedf336f1e4b5f8cf11a39e7eb
name <swlee1112@naver.com>
2023-08-20 16:53:55 +0900

    Create BOJ_2577.cs

---
+++ BOJ_2577.cs
@@ -0,0 +1,14 @@
+﻿// BOJ_2577
+
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int mul = a * b * c;
+string tostring = mul.ToString();
+int[] arr = new int[10];
+for (int i = 0; i <tostring.Length; i++)
+    arr[tostring[i] - '0']++;
+
+for (int i = 0; i < 10; i++)
+    Console.WriteLine(arr[i]);
