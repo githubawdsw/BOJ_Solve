b62a3c43d66fa96d917a4883511764da6349b696
name <swlee1112@naver.com>
2023-08-20 16:53:51 +0900

    Create BOJ_2231.cs

---
+++ BOJ_2231.cs
@@ -0,0 +1,23 @@
+﻿// BOJ_2231
+
+
+int n = int.Parse(Console.ReadLine());
+
+
+int ans = 1;
+while (ans < n)
+{
+    string temps = ans.ToString();
+    int tempint = ans;
+    for (int i = 0; i < temps.Length; i++)
+    {
+        tempint += temps[i] - '0';
+    }
+    if(tempint == n)
+    {
+        Console.WriteLine( ans );
+        return;
+    }
+    ans++;
+}
+Console.WriteLine( 0 );
