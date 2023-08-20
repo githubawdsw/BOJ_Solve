0d719aeb04986db6698784d2fe044ee55be35f7c
name <swlee1112@naver.com>
2023-08-20 16:53:53 +0900

    Create BOJ_2292.cs

---
+++ BOJ_2292.cs
@@ -0,0 +1,18 @@
+﻿// BOJ_2292
+
+
+int sector = 1;
+int count = 0;
+int n = int.Parse(Console.ReadLine());
+for (int i = 0;  sector < n; i++)
+{
+    sector += i * 6;
+    count++;
+}
+if (n ==1)
+{
+    Console.Write(1);
+}
+else
+Console.Write(count);
+
