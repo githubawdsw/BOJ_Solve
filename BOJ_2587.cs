4af6372db7a8df0e207b5b8b48cd6727c1af61f5
name <swlee1112@naver.com>
2023-08-21 19:27:44 +0900

    Create BOJ_2587.cs

---
+++ BOJ_2587.cs
@@ -0,0 +1,16 @@
+﻿// BOJ_2587
+
+
+List<int> list = new List<int>();
+int sum = 0;
+for (int i = 0; i < 5; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    list.Add(input);
+    sum += input;
+}
+
+list.Sort();
+Console.WriteLine(  sum / 5);
+Console.WriteLine(list[2]);
+
