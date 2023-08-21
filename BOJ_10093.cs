a727ef636a5145f17a3c5030078ec42dba50cf99
name <swlee1112@naver.com>
2023-08-21 19:27:40 +0900

    Create BOJ_10093.cs

---
+++ BOJ_10093.cs
@@ -0,0 +1,16 @@
+﻿// BOJ_10093
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+string[] inputab = Console.ReadLine().Split(' ');
+long a = Math.Min( long.Parse(inputab[0]) , long.Parse(inputab[1]));
+long b = Math.Max(long.Parse(inputab[0]), long.Parse(inputab[1]));
+List<long> list = new List<long>();
+for (long i = a + 1; i < b; i++)
+    list.Add(i);
+
+sb.AppendLine(list.Count.ToString());
+for (int i = 0; i < list.Count; i++)
+    sb.Append($"{list[i]} ");
+Console.WriteLine(sb);
