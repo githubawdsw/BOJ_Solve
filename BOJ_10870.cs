25d3c0b5a34a89fa561db2f86a52f5caaa656fd5
name <swlee1112@naver.com>
2023-08-22 19:37:41 +0900

    Create BOJ_10870.cs

---
+++ BOJ_10870.cs
@@ -0,0 +1,20 @@
+﻿//BOJ_10870
+
+
+int n = int.Parse(Console.ReadLine());
+if(n == 0)
+{
+    Console.WriteLine(  0 );
+    return;
+}
+int ans = Fibonacci(n);
+Console.WriteLine(  ans );
+
+
+int Fibonacci(int num)
+{
+    if (num == 1 || num == 2)
+        return 1;
+    return Fibonacci(num - 2) + Fibonacci(num - 1);
+}
+
