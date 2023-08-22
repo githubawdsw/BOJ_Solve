0a35a30183ded632e151f05dc6267691f842ff58
name <swlee1112@naver.com>
2023-08-22 19:37:39 +0900

    Create BOJ_10809.cs

---
+++ BOJ_10809.cs
@@ -0,0 +1,29 @@
+﻿// BOJ_10809
+
+
+using System.Text;
+
+string s = Console.ReadLine();
+
+StringBuilder sb = new StringBuilder();
+for (int i = 0; i < 26; i++)
+{
+    char c = (char)(i + 97);
+	bool check = false;
+	int idx = 0;
+	for (int j = 0; j < s.Length; j++)
+	{
+		if (s[j] == c)
+		{
+			check = true;
+			idx = j;
+			break;
+		}
+	}
+	if (check)
+		sb.Append($"{idx} ");
+	else
+		sb.Append($"-1 ");
+}
+Console.WriteLine(	sb);
+
