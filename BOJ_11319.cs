a917975010d64df1b04f48079371720b8ca30ef7
name <swlee1112@naver.com>
2023-08-22 19:37:49 +0900

    Create BOJ_11319.cs

---
+++ BOJ_11319.cs
@@ -0,0 +1,25 @@
+﻿// BOJ_11319
+
+
+
+int t = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < t; i++)
+{
+    string inputs = Console.ReadLine();
+    int a = 0, b = 0, c = 0;
+    for (int j = 0; j < inputs.Length; j++)
+    {
+
+        if (inputs[j] == 'A' || inputs[j] == 'E' || inputs[j] == 'I' || inputs[j] == 'O' || inputs[j] == 'U'
+             || inputs[j] == 'a' || inputs[j] == 'e' || inputs[j] == 'i' || inputs[j] == 'o' || inputs[j] == 'u')
+            a++;
+        else
+            b++;
+        if (inputs[j] == ' ')
+            b--;
+    }
+    Console.WriteLine(  $"{b} {a}");
+}
+        
