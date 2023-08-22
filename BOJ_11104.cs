a21f7168b4a808e5013012e1b0aef6877ff83a2a
name <swlee1112@naver.com>
2023-08-22 19:37:47 +0900

    Create BOJ_11104.cs

---
+++ BOJ_11104.cs
@@ -0,0 +1,10 @@
+﻿// BOJ_11104
+
+
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string inputbinary = Console.ReadLine();
+    Console.WriteLine(  Convert.ToInt32(inputbinary , 2));
+}
+
