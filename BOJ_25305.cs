f3f5ff75d2769af92957693491853d1c7d4f23f0
name <swlee1112@naver.com>
2023-08-24 03:14:48 +0900

    Create BOJ_25305.cs

---
+++ BOJ_25305.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Baekjoon1
+{
+    
+    class BOJ_25305
+    {
+        static void Main(string[] args)
+        {
+            string[] inputnk = Console.ReadLine().Split(' ');
+            int n = int.Parse(inputnk[0]);
+            int k = int.Parse(inputnk[1]);
+
+            string[] inputX = Console.ReadLine().Split(' ');
+            List<int> X = new List<int>();
+            for (int i = 0; i < inputX.Length; i++)
+                X.Add(int.Parse(inputX[i]));
+            X.Sort();
+            Console.WriteLine(X[n -k]);
+        }
+    }
+    
+}
