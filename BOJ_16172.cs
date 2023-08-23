cc6d979f342822984938ebcf4fa56c41e3418a68
name <swlee1112@naver.com>
2023-08-24 03:14:40 +0900

    Create BOJ_16172.cs

---
+++ BOJ_16172.cs
@@ -0,0 +1,39 @@
+﻿//  BOJ_16172
+
+
+string s = Console.ReadLine();
+
+string k = Console.ReadLine();
+int[] f = failureFuc(k);
+
+int j = 0;
+for (int i = 0; i < s.Length; i++)
+{
+	if (s[i] >= '0' && s[i] <= '9')
+		continue;
+	while (j > 0 && s[i] != k[j])
+		j = f[j - 1];
+	if (s[i] == k[j])
+		j++;
+	if(j == k.Length)
+	{
+        Console.WriteLine(	1);
+        return;
+	}
+}
+Console.WriteLine(	0);
+
+
+int[] failureFuc(string str)
+{
+	int[] failure = new int[str.Length];
+	int j = 0;
+	for (int i = 1; i < str.Length; i++)
+	{
+		while (j > 0 && str[i] != str[j])
+			j = failure[j - 1];
+		if (str[i] == str[j])
+			failure[i] = ++j;
+	}
+	return failure;
+}
