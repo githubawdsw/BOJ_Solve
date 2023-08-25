c6f2da7cb5249f7f46f7f6d2b20b99016c47ed1b
name <swlee1112@naver.com>
2023-08-25 13:57:43 +0900

    Create BOJ_1259.cs

---
+++ BOJ_1259.cs
@@ -0,0 +1,30 @@
+﻿// BOJ_1259
+
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input == "0")
+        break;
+    int left = 0;
+    int right = input.Length - 1;
+    bool check = true;
+
+    while (left < right)
+    {
+        if (input[left] != input[right]) 
+            check = false;
+        left++;
+        right--;
+    }
+    if (check)
+        sb.AppendLine("yes");
+    else
+        sb.AppendLine("no");
+
+}
+Console.WriteLine(  sb);
+
