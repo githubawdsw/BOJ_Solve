1bae6bc1722b0f76106742d6b34371c72e7e2c7e
name <swlee1112@naver.com>
2023-08-25 13:57:46 +0900

    Create BOJ_1292.cs

---
+++ BOJ_1292.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_1292
+    {
+        static void Main(string[] args)
+        {
+            string[] inputab = Console.ReadLine().Split(' ');
+            int a = int.Parse(inputab[0]);
+            int b = int.Parse(inputab[1]);
+
+            List<int> arrlist = new List<int>();
+            for (int i = 1; i <= b; i++)
+                for (int j = 1; j <= i; j++)
+                    arrlist.Add(i);
+
+            int ans = 0;
+            for (int i = a-1; i < b; i++)
+                ans += arrlist[i];
+
+            Console.WriteLine(ans);
+        }
+    }
+    
+}
