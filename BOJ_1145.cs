68677817ffe1749d994daec543e08b3bf1d802b1
name <swlee1112@naver.com>
2023-08-25 13:57:40 +0900

    Create BOJ_1145.cs

---
+++ BOJ_1145.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_1145
+    {
+        static void Main(string[] args)
+        {
+            string[] inputarr = Console.ReadLine().Split(' ');
+
+            int num = 1;
+            while (true)
+            {
+                int count = 0;
+                for (int i = 0; i < inputarr.Length; i++)
+                {
+                    if (num >= int.Parse(inputarr[i]) && num % int.Parse(inputarr[i]) == 0)
+                        count++;
+                }
+                if (count >= 3)
+                {
+                    Console.WriteLine(  num);
+                    break;
+                }
+                num++;
+            }
+        }
+
+    }
+    
+}
