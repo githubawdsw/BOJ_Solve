61ca583cbcc482dbedbcdeb87f9e625dc264103c
name <swlee1112@naver.com>
2023-08-27 15:25:46 +0900

    Create BOJ_1526.cs

---
+++ BOJ_1526.cs
@@ -0,0 +1,23 @@
+﻿// BOJ_1526
+
+
+int n = int.Parse(Console.ReadLine());
+
+int ans = 0;
+for (int i = 4; i <= n; i++)
+{
+    string num = i.ToString();
+	bool check = true;
+	for (int j = 0; j < num.Length; j++)
+	{
+		if (num[j] != '4' && num[j] != '7')
+		{
+			check = false;
+			break;
+		}
+	}
+    if (check)
+		ans = i;
+}
+Console.WriteLine(	ans );
+
