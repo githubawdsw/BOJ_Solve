9954f66477a5ef786e5ca7a677346c6d8bf8e749
name <swlee1112@naver.com>
2023-08-28 19:05:27 +0900

    Create BOJ_10798.cs

---
+++ BOJ_10798.cs
@@ -0,0 +1,24 @@
+﻿// BOJ_10798
+
+
+List< string> list = new List< string>();
+int maxLength = 0;
+for (int i = 0; i < 5; i++)
+{
+    string input = Console.ReadLine();
+    list.Add(input);
+    maxLength = Math.Max(maxLength, input.Length);
+}
+
+string ans = "";
+for (int i = 0; i < maxLength; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        string str = list[j];
+        if (i >= str.Length)
+            continue;
+        ans += str[i];
+    }
+}
+Console.WriteLine(  ans);
