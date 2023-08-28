1b62e8c77638ca5a4d049de25416dd1680e505cf
name <swlee1112@naver.com>
2023-08-28 19:05:29 +0900

    Create BOJ_2775.cs

---
+++ BOJ_2775.cs
@@ -0,0 +1,24 @@
+﻿// BOJ_2775
+
+
+
+using System.Text;
+
+StringBuilder sb=  new StringBuilder();
+int t = int.Parse(Console.ReadLine());
+while (t-- > 0)
+{
+    int k = int.Parse(Console.ReadLine());  
+    int n = int.Parse(Console.ReadLine());
+    int[,] dp = new int[15, 15];
+    for (int i = 1; i <= n; i++)
+        dp[0, i] = i;
+
+    for (int i = 1; i <= k; i++)
+        for (int j = 1; j <= n; j++)
+            for (int h = 1; h <= j; h++)
+                dp[i, j] += dp[i - 1, h];
+    sb.AppendLine(dp[k, n].ToString());
+}
+Console.WriteLine(  sb);
+
