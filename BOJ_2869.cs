6d375888dd1d7c93b79094c6300251aaf07fd20d
name <swlee1112@naver.com>
2023-08-28 19:05:30 +0900

    Create BOJ_2869.cs

---
+++ BOJ_2869.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_2869
+    {
+        static void Main(string[] args)
+        {
+            string[] inputABV = Console.ReadLine().Split(' ');
+            int a, b, v;
+            a = int.Parse(inputABV[0]);
+            b = int.Parse(inputABV[1]);
+            v = int.Parse(inputABV[2]);
+            int count = 0;
+
+            count = (v - b) / (a - b);
+            if ((v - b) % (a - b) > 0)
+                count++;
+            Console.WriteLine(count);
+        }
+    }
+    
+}
