0b1e4ebf3efe0f4b5b1e121da876062b6a8aab48
name <swlee1112@naver.com>
2023-08-29 16:53:55 +0900

    Create BOJ_11653.cs

---
+++ BOJ_11653.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+namespace MathAlgorithm
+{
+    
+    class BOJ_11653
+    {
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int divide = 2;
+            
+            while (n != 1)
+            {
+                if (n % divide == 0)
+                {
+                    n /= divide;
+                    sb.AppendLine(divide.ToString());
+                }
+                else
+                    divide++;
+                if (divide * divide > n)
+                    break;
+            }
+            if(n != 1)
+                sb.AppendLine(n.ToString());
+            Console.WriteLine(sb);
+        }
+    }
+    
+}
