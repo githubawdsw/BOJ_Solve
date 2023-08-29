e5d27a475ecfd9f484269816f745b4c3c59d3e0a
name <swlee1112@naver.com>
2023-08-29 16:54:00 +0900

    Create BOJ_24416.cs

---
+++ BOJ_24416.cs
@@ -0,0 +1,32 @@
+﻿// BOJ_24416
+
+
+int n = int.Parse(Console.ReadLine());
+
+int acount = 0;
+int bcount = 0;
+acount =  fib(n);
+
+int[] f = new int[45];
+f[1] = 1;
+f[2] = 2;
+
+fibonaci(n);
+Console.WriteLine(  $"{acount } {bcount}");
+
+int fib( int n)
+{
+    if (n == 1 || n == 2)
+        return 1;
+    else
+        return (fib(n - 1) + fib(n - 2));
+}
+
+void fibonaci(int n)
+{
+    for (int i = 3; i <= n; i++)
+    {
+        f[i] = f[i - 1] + f[i - 2];
+        bcount++;
+    }
+}
