96228e1dd88e91bcaf722854987c2960ef288ea2
name <swlee1112@naver.com>
2023-08-30 18:43:35 +0900

    Create BOJ_1193.cs

---
+++ BOJ_1193.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MathAlgorithm
+{
+    
+    class BOJ_1193
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int denominator;
+            int numerator;
+
+            int i = 1;
+            while (n > i)
+            {
+                n -= i;
+                i++;
+            }
+            denominator = i - n + 1;
+            numerator = n;
+            if (i % 2 == 1)
+            {
+                int swap = denominator;
+                denominator = numerator;
+                numerator = swap;
+            }
+
+            Console.WriteLine($"{numerator}/{denominator}");
+        }
+    }
+    
+}
