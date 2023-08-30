f65cc30c534abddd64e0cf96bc7a3a41692982ac
name <swlee1112@naver.com>
2023-08-30 18:43:38 +0900

    Create BOJ_1427.cs

---
+++ BOJ_1427.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Sorting
+{
+    
+    class BOJ_1427
+    {
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine();
+            N = new string(N.OrderByDescending(x => x).ToArray());
+            Console.WriteLine( N );
+        }
+    }
+    
+}
