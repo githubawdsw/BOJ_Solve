72a11772b991fddda731cd8ff65eca3b01e64913
name <swlee1112@naver.com>
2023-08-30 18:43:43 +0900

    Create BOJ_1475.cs

---
+++ BOJ_1475.cs
@@ -0,0 +1,18 @@
+﻿
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+string n = sr.ReadLine();
+int[] arr = new int[9];
+for (int i = 0; i < n.Length; i++)
+{
+    if (n[i] == '6' || n[i] == '9')
+        arr[6]++;
+    else
+        arr[int.Parse(n[i].ToString())]++;
+}
+if (arr[6] % 2 == 1)
+    arr[6] = (arr[6] + 1) / 2;
+else
+    arr[6] = arr[6]  / 2;
+Console.WriteLine(arr.Max());
