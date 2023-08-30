f270291471e8a5c621164c172a970fc550ca14af
name <swlee1112@naver.com>
2023-08-30 18:43:42 +0900

    Create BOJ_1439.cs

---
+++ BOJ_1439.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GreedyAlgorithm
+{
+    
+    class BOJ_1439
+    {
+        static int n;
+        static void Main(string[] args)
+        {
+            string inputarr = Console.ReadLine();
+
+            int count = 0;
+            for (int i = 1; i < inputarr.Length; i++)
+            {
+                if (inputarr[0] != inputarr[i])
+                {
+                    if (inputarr[i] != inputarr[i - 1])
+                        count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+    
+}
