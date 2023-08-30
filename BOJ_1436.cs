5ad01e44d3116b1b52bb62e275aa7f78c58e721f
name <swlee1112@naver.com>
2023-08-30 18:43:40 +0900

    Create BOJ_1436.cs

---
+++ BOJ_1436.cs
@@ -0,0 +1,18 @@
+﻿// BOJ_1436
+
+
+
+int n = int.Parse(Console.ReadLine());
+
+int count = 0;
+for (int i = 666; i < 2700000; i++)
+{
+    if (i.ToString().Contains("666"))
+        count++;
+    if (count == n)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+}
+
