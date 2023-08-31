bb970a2ba93d663ff6969a92586d1e8f94966408
name <swlee1112@naver.com>
2023-08-31 19:15:40 +0900

    Create BOJ_1543.cs

---
+++ BOJ_1543.cs
@@ -0,0 +1,36 @@
+﻿// BOJ_1543
+
+
+string n = Console.ReadLine();
+string find = Console.ReadLine();
+
+int count = 0;
+int idx = 0;
+bool flag = false;
+for (int i = 0; i < n.Length; i++)
+{
+    for (int j = i; j < n.Length; j++)
+    {
+        if (n[j] == find[idx])
+            idx++;
+        else
+        {
+            idx = 0;
+            break;
+        }
+        if (idx == find.Length)
+        {
+            count++;
+            flag = true;
+            idx = 0;
+            break;
+        }
+    }
+    idx = 0;
+    if (flag)
+    {
+        i += find.Length - 1;
+        flag = false;
+    }
+}
+Console.WriteLine(count);
