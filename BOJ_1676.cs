df1108a38ef90abf23721ae7ec48dd64afa0440f
name <swlee1112@naver.com>
2023-08-31 19:15:43 +0900

    Create BOJ_1676.cs

---
+++ BOJ_1676.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_1676
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            while(n > 1)
+            {
+                count += n / 5;
+                n /= 5;
+            }
+            Console.WriteLine(count);
+        }
+    }
+    
+}
