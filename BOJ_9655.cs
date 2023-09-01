649e1ddd75786f098881d5b7ac0a231e9b20a0cc
name <swlee1112@naver.com>
2023-09-01 18:33:50 +0900

    Create BOJ_9655.cs

---
+++ BOJ_9655.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Dynamic
+{
+    
+    class BOJ_9655
+    {
+        static int[,] d = new int[1005, 5];
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if(n % 2 == 1)
+                Console.WriteLine("SK");
+            else
+                Console.WriteLine("CY");
+        }
+    }
+    
+}
