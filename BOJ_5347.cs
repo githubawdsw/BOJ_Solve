7085477f59f501cdd28f7a6c9b2e156b04ef9016
name <swlee1112@naver.com>
2023-09-01 18:33:43 +0900

    Create BOJ_5347.cs

---
+++ BOJ_5347.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+namespace MathAlgorithm
+{
+    
+    class BOJ_5347
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+
+            while (n > 0)
+            {
+                n--;
+                string[] inputab = Console.ReadLine().Split(' ');
+                long a = long.Parse(inputab[0]);
+                long b = long.Parse(inputab[1]);
+
+                long LCM = (a * b )/GCD(a , b);
+                sb.AppendLine(LCM.ToString());
+            }
+            Console.WriteLine(sb);
+        }
+
+        static long GCD(long a, long b)
+        {
+            if (b == 0)
+                return a;
+            return GCD(b, (a % b));
+        }
+    }
+    
+}
