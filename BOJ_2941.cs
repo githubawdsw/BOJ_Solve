3f64295c448453d57429650cf824fa7880c09017
name <swlee1112@naver.com>
2023-09-01 18:33:42 +0900

    Create BOJ_2941.cs

---
+++ BOJ_2941.cs
@@ -0,0 +1,12 @@
+﻿// BOJ_2941
+
+
+string inputstr = Console.ReadLine();
+string[] str = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=" , "z=" };
+for (int i = 0; i < str.Length; i++)
+{
+    if (inputstr.Contains(str[i]))
+        inputstr = inputstr.Replace(str[i], ".");
+}
+Console.WriteLine(  inputstr.Length);
+
