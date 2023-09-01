275e4b2f79249507f01acd503f6cc95f405c209d
name <swlee1112@naver.com>
2023-09-02 05:23:30 +0900

    Create BOJ_10815.cs

---
+++ BOJ_10815.cs
@@ -0,0 +1,26 @@
+﻿//BOJ_10815
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+
+int n = int.Parse(Console.ReadLine());
+string[] inputn = Console.ReadLine().Split(' ');
+List<int> nlist = new List<int>();
+
+for (int i = 0; i < n; i++)
+    nlist.Add(int.Parse(inputn[i]));
+
+int m = int.Parse(Console.ReadLine());
+string[] inputm = Console.ReadLine().Split(' ');
+
+int[] countingsort = new int[20000001];
+for (int i = 0; i < n; i++)
+    countingsort[nlist[i] + 10000000]++;
+
+for (int i = 0; i < m; i++)
+    sb.Append($"{countingsort[int.Parse(inputm[i]) + 10000000]} ");
+
+Console.WriteLine(sb);
+
+
