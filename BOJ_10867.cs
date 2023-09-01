f23880f8f43099c2152155acff53b936bd25d1d6
name <swlee1112@naver.com>
2023-09-02 05:23:32 +0900

    Create BOJ_10867.cs

---
+++ BOJ_10867.cs
@@ -0,0 +1,22 @@
+﻿// BOJ_10867
+
+using System.Text;
+    
+StringBuilder sb = new StringBuilder();
+
+int n = int.Parse(Console.ReadLine());
+string[] inputArr = Console.ReadLine().Split(' ');
+List<int> arrList = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    int val = int.Parse(inputArr[i]);
+    if (!arrList.Contains(val))
+        arrList.Add(val);
+}
+
+arrList.Sort();
+for (int i = 0; i < arrList.Count; i++)
+    sb.Append($"{arrList[i]} ");
+
+Console.WriteLine(sb);
