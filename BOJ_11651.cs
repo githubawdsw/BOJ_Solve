2b1f62335a20d69a53d8bc4a98a7c0e81d7412d3
name <swlee1112@naver.com>
2023-09-03 20:05:31 +0900

    Create BOJ_11651.cs

---
+++ BOJ_11651.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting
+{
+    
+    class BOJ_11651
+    {
+        static int N;
+        static List<Tuple<int, int>> inputArr = new List<Tuple<int, int>>();
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputValue = Console.ReadLine().Split(' ');
+                inputArr.Add(new Tuple<int, int>(int.Parse(inputValue[0]), int.Parse(inputValue[1])));
+            }
+                
+            inputArr = inputArr.OrderBy(x => x.Item2).ThenBy(y => y.Item1).ToList();
+            for (int i = 0; i < N; i++)
+            {
+                sb.AppendLine($"{inputArr[i].Item1} {inputArr[i].Item2}");
+            }
+            Console.WriteLine(sb);
+            
+        }
+    }
+
+}
