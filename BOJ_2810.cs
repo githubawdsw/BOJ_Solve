f397787db22cba6428c19a38b7a676abff771de5
name <swlee1112@naver.com>
2023-09-03 20:05:38 +0900

    Create BOJ_2810.cs

---
+++ BOJ_2810.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace GreedyAlgorithm
+{
+    
+    class BOJ_2810
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string str = Console.ReadLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+                if (str[i] == 'L')
+                {
+                    count++;
+                    i++;
+                }
+
+            if(count == 0)
+                Console.WriteLine(n);
+            else
+                Console.WriteLine( n - count + 1 );
+        }
+    }
+    
+}
