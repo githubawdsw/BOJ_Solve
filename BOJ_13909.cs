7b7f1d14f73e417b14d7b98b91afbf59efe8bfa0
name <swlee1112@naver.com>
2023-09-04 21:05:21 +0900

    Create BOJ_13909.cs

---
+++ BOJ_13909.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_13909
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i * i <= n; i++)
+                count++;
+            Console.WriteLine(  count);
+        }
+    }
+    
+}
