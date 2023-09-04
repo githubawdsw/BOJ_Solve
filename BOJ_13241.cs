2ca14e4d517f32b33e2a020e89ac3511b288dfcf
name <swlee1112@naver.com>
2023-09-04 21:05:17 +0900

    Create BOJ_13241.cs

---
+++ BOJ_13241.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_13241
+    {
+        static void Main(string[] args)
+        {
+            string[] inputab = Console.ReadLine().Split(' ');
+            long a = long.Parse(inputab[0]);
+            long b = long.Parse(inputab[1]);
+            long ans = a * b / GCD(a, b);
+            Console.WriteLine(  ans);
+        }
+
+        static long GCD(long a, long b)
+        {
+            if (b == 0)
+                return a;
+            return GCD(b, a % b);
+        }
+    }
+    
+}
