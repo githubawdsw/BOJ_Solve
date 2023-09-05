3e7c1541a2f5d27185d775290768da25adfa4fea
name <swlee1112@naver.com>
2023-09-05 18:24:21 +0900

    Create BOJ_24313.cs

---
+++ BOJ_24313.cs
@@ -0,0 +1,32 @@
+﻿// BOJ_24313
+
+
+
+string[] an =  Console.ReadLine().Split(' ');
+int a1 = int.Parse(an[0]);
+int a0 = int.Parse(an[1]);
+int c = int.Parse(Console.ReadLine());
+int n0 = int.Parse(Console.ReadLine());
+
+int fn = a1 * n0 + a0;
+int gn = c * n0;
+bool check = false;
+while (fn <= gn)
+{
+    n0++;
+    fn = a1 * n0 + a0;
+    gn = c * n0;
+    check = true;
+    if(fn > gn)
+    {
+        check = false;
+        break;
+    }
+    if (gn > 10000 )
+        break;
+}
+if (check)
+    Console.WriteLine(1);
+else
+    Console.WriteLine(0);
+
