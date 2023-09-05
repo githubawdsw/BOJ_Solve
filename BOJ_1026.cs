518a0363547f27ee3c32b3c320a1e303f574f416
name <swlee1112@naver.com>
2023-09-05 18:24:17 +0900

    Create BOJ_1026.cs

---
+++ BOJ_1026.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace GreedyAlgorithm
+{
+    
+    class BOJ_1026
+    {
+        static int n;
+        static int[] A = new int[55];
+        static int[] B = new int[55];
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            string[] inputA = Console.ReadLine().Split(' ');
+            string[] inputB = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                A[i] = int.Parse(inputA[i]);
+                B[i] = int.Parse(inputB[i]);
+            }
+            Array.Sort(A, 0, n);
+            Array.Sort(B, 0, n);
+
+            int S = 0;
+            for (int i = 0; i < n; i++)
+                S += A[i] * B[n - i - 1];
+            Console.WriteLine(S);
+        }
+    }
+    
+}
