0ab36f51c44b87e84e2ebbedeeea2e1634b4ac09
name <swlee1112@naver.com>
2023-09-06 16:35:52 +0900

    Create BOJ_2851.cs

---
+++ BOJ_2851.cs
@@ -0,0 +1,13 @@
+﻿// BOJ_2851
+
+
+int ans = 0;
+for (int i = 0; i < 10; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    if (Math.Abs(100 - ans) >= Math.Abs(100 - (ans + input)))
+        ans += input;
+    else
+        break;
+}
+Console.WriteLine(ans);
