d3652d216c56ae1bad1f570fbe1aee0fa4a9f227
name <swlee1112@naver.com>
2023-09-06 16:35:44 +0900

    Create BOJ_1057.cs

---
+++ BOJ_1057.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_1057
+    {
+        static void Main(string[] args)
+        {
+            string[] inputval = Console.ReadLine().Split(' ');
+            int n = int.Parse(inputval[0]);
+            int k = int.Parse(inputval[1]);
+            int l = int.Parse(inputval[2]);
+
+            int count = 0;
+            while (k != l)
+            {
+                k = (k + 1) / 2;
+                l = (l + 1) / 2;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+    
+}
