f6051db5975f378d4e402dc4854c4d1615336faf
name <swlee1112@naver.com>
2023-09-06 16:35:42 +0900

    Create BOJ_1049.cs

---
+++ BOJ_1049.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GreedyAlgorithm
+{
+    
+    class BOJ_1049
+    {
+        static void Main(string[] args)
+        {
+            string[] inputnm = Console.ReadLine().Split(' ');
+            int n = int.Parse(inputnm[0]);
+            int m = int.Parse(inputnm[1]);
+
+            int minp = int.MaxValue;
+            int mine = int.MaxValue;
+            int cost = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] inputcost = Console.ReadLine().Split(' ');
+                int package = int.Parse(inputcost[0]);
+                int each = int.Parse(inputcost[1]);
+                minp = Math.Min(minp, package);
+                mine = Math.Min(mine, each);
+            }
+
+            if(n > 6)
+            {
+                if (minp < mine * 6)
+                    cost += minp * (n / 6);
+                else
+                    cost += mine * 6 * (n / 6);
+                n %= 6;
+            }
+            if (mine * n > minp)
+                cost += minp;
+            else
+                cost += mine * n;
+            Console.WriteLine(  cost);
+        }
+    }
+    
+}
