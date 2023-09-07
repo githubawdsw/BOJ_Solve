f1f54211381b6360061376395d6411c01255d6e4
name <swlee1112@naver.com>
2023-09-07 16:01:39 +0900

    Create BOJ_1764.cs

---
+++ BOJ_1764.cs
@@ -0,0 +1,31 @@
+﻿// BOJ_1764
+
+
+using System.Text;
+
+string[] inputnm = Console.ReadLine().Split(' ');
+int n = int.Parse(inputnm[0]);
+int m = int.Parse(inputnm[1]);
+
+List<string> nlist = new List<string>();
+SortedSet<string> ans = new SortedSet<string>();
+for (int i = 0; i < n; i++)
+{
+    string inputstr = Console.ReadLine();
+    nlist.Add(inputstr);
+}
+nlist.Sort();
+for (int i = 0; i < m; i++)
+{
+    string inputstr = Console.ReadLine();
+    int idx = nlist.BinarySearch(inputstr);
+    if (idx >= 0 && idx < n)
+        ans.Add(inputstr);
+}
+StringBuilder sb = new StringBuilder();
+sb.AppendLine(ans.Count.ToString());
+foreach (var one in ans)
+    sb.AppendLine(one);
+
+Console.WriteLine(  sb);
+
