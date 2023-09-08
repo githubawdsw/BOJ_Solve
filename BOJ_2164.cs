77e774fb2ad2fdc9b7a97717b7c261103ee5e748
name <swlee1112@naver.com>
2023-09-08 14:48:59 +0900

    Create BOJ_2164.cs

---
+++ BOJ_2164.cs
@@ -0,0 +1,18 @@
+﻿// BOJ_2164
+    
+
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int n = int.Parse(sr.ReadLine());
+Queue<int> q = new Queue<int>();
+for (int i = 1; i <= n; i++)
+    q.Enqueue(i);
+for (int i = 1; q.Count != 1; i++)
+{
+    if (i % 2 == 1)
+        q.Dequeue();
+    else
+        q.Enqueue(q.Dequeue());
+}
+Console.WriteLine(q.Peek());
