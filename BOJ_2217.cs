dbaa641815cea024b2af9118a909a36ad446831b
name <swlee1112@naver.com>
2023-09-08 14:49:01 +0900

    Create BOJ_2217.cs

---
+++ BOJ_2217.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GreedyAlgorithm
+{
+    
+    class BOJ_2217
+    {
+        static int n;
+        static List<int> inputlope = new List<int>();
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                inputlope.Add(int.Parse(Console.ReadLine()));
+
+            inputlope.Sort();
+            int ans = 0;
+            for (int i = 1; i <= n; i++)
+                ans = Math.Max(ans, inputlope[n - i] * i);
+            
+            Console.WriteLine(ans);
+        }
+    }
+    
+}
