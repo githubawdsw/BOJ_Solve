89444ab8270e784fe7771537e01ddf64792e96f5
name <swlee1112@naver.com>
2023-09-08 14:49:06 +0900

    Create BOJ_2839.cs

---
+++ BOJ_2839.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_2839
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (n >= 0)
+            {
+                if (n % 5 == 0)
+                {
+                    count += n / 5;
+                    Console.WriteLine(count);
+                    return;
+                }
+                
+                n -= 3;
+                count++;
+                
+            }
+            Console.WriteLine(-1);
+            
+        }
+    }
+    
+}
