48a642970f37b1700dbb0c57fd0018c5d51b352a
name <swlee1112@naver.com>
2023-09-09 16:29:20 +0900

    Create BOJ_4949.cs

---
+++ BOJ_4949.cs
@@ -0,0 +1,44 @@
+﻿// BOJ_4949
+
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+while (true)
+{
+    Stack<char> s = new Stack<char>();
+    string inputstring = Console.ReadLine();
+    if (inputstring == ".")
+        break;
+    int idx = 0;
+    char top = ' ';
+    bool flag = false;
+    while (idx < inputstring.Length)
+    {
+        if (inputstring[idx] == '[' || inputstring[idx] == '(')
+            s.Push(inputstring[idx]);
+
+        else if (inputstring[idx] == ']' || inputstring[idx] == ')')
+        {
+            if(s.Count == 0 )
+            {
+                flag = true;
+                break;
+            }
+
+            top = s.Peek();
+            if ((inputstring[idx] == ']' && top != '[') || (inputstring[idx] == ')' && top != '('))
+            {
+                flag = true;
+                break;
+            }
+            s.Pop();
+        }
+        idx++;
+    }
+    if (flag || s.Count != 0)
+        sb.AppendLine("no");
+    else
+        sb.AppendLine("yes");
+}
+Console.WriteLine(sb);
