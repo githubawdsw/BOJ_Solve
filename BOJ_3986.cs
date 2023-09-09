9ecf21d1795bb7b634a1c9b159e652d5aaf5276e
name <swlee1112@naver.com>
2023-09-09 16:29:16 +0900

    Create BOJ_3986.cs

---
+++ BOJ_3986.cs
@@ -0,0 +1,26 @@
+﻿// BOJ_3986
+  
+
+int n = int.Parse(Console.ReadLine());
+int ans = 0;
+while (n-- > 0)
+{
+    string inputstring = Console.ReadLine();
+    Stack<char> s = new Stack<char>();
+    int idx = 0;
+    char top = ' ';
+    while (idx < inputstring.Length)
+    {
+        if (s.Count == 0 || top != inputstring[idx])
+            s.Push(inputstring[idx]);
+        else if (top == inputstring[idx])
+            s.Pop();
+        if (s.Count != 0)
+            top = s.Peek();
+        idx++;
+    }
+
+    if (s.Count == 0)
+        ans++;
+}
+Console.WriteLine(ans);
