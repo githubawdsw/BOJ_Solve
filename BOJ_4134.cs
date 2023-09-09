1437a15f9eca0c0e12888df70eafa182c8917d7f
name <swlee1112@naver.com>
2023-09-09 16:29:17 +0900

    Create BOJ_4134.cs

---
+++ BOJ_4134.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_4134
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                long n = long.Parse(Console.ReadLine());
+                if(n == 1 || n == 0)
+                {
+                    sb.AppendLine("2");
+                    continue;
+                }
+                for (long i = n; i <= long.MaxValue; i++)
+                {
+                    bool check = true;
+                    for (long j = 2; j * j <= i; j++)
+                    {
+                        if(i % j == 0)
+                        {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if(check)
+                    {
+                        sb.AppendLine(i.ToString());
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(  sb);
+        }
+    }
+    
+}
