8c3a6a2a8db537e2a0d843db8ca9ab1dc082bbe3
name <swlee1112@naver.com>
2023-09-10 18:22:08 +0900

    Create BOJ_10610.cs

---
+++ BOJ_10610.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_10610
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+
+            char[] c = n.ToCharArray();
+            Array.Sort(c);
+
+            if (c[0] != '0')
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+
+            int checkthree = 0;
+            for (int i = 1; i < c.Length; i++)
+                checkthree += c[i] - '0';
+            if(checkthree % 3 != 0)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+            Array.Reverse(c);
+            Console.WriteLine(c);
+        }
+    }
+    
+}
