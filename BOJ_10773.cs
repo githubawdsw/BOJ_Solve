9b9dde64e7d2062556306daa9d1bf8608354a8ab
name <swlee1112@naver.com>
2023-09-10 18:22:10 +0900

    Create BOJ_10773.cs

---
+++ BOJ_10773.cs
@@ -0,0 +1,18 @@
+﻿// BOJ_10773
+
+Stack<int> s = new Stack<int>();
+int k = int.Parse(Console.ReadLine());
+for (int i = 0; i < k; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    if (n == 0)
+        s.Pop();
+    else
+        s.Push(n);
+}
+
+int ans = 0;
+while (s.Count > 0)
+    ans += s.Pop();
+
+Console.WriteLine(ans);
