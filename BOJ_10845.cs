2be32566431fed6b39e927319af597f3b2d00f58
name <swlee1112@naver.com>
2023-09-11 17:54:16 +0900

    Create BOJ_10845.cs

---
+++ BOJ_10845.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace StackAndQueue
+{
+
+    class BOJ_10845
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse( Console.ReadLine());
+            Queue<int> Q = new Queue<int>();
+            StringBuilder sb = new StringBuilder();
+            int QbackIndex;
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] str = Console.ReadLine().Split(' ');
+                if (str[0] == "push")
+                    Q.Enqueue(int.Parse(str[1]));
+
+                else if(str[0] == "pop")
+                {
+                    if (Q.Count == 0)
+                        sb.AppendLine("-1");
+                    else
+                    {
+                        sb.Append(Q.Peek() + "\n");
+                        Q.Dequeue();
+                    }
+                }
+
+                else if (str[0] == "size")
+                    sb.Append(Q.Count + "\n");
+
+                else if (str[0] == "empty")
+                {
+                    if (Q.Count == 0)
+                        sb.AppendLine("1");
+                    else
+                        sb.AppendLine("0");
+                }
+
+                else if (str[0] == "front")
+                {
+                    if (Q.Count == 0)
+                        sb.AppendLine("-1");
+                    else
+                        sb.Append(Q.Peek() + "\n");
+                }
+
+                else if (str[0] == "back")
+                {
+                    if (Q.Count == 0)
+                        sb.AppendLine("-1");
+                    else
+                    {
+                        QbackIndex = Q.Count - 1;
+                        sb.Append(Q.Last() + "\n");
+                    }
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+
+}
