4328f14e90ee2e1514e22007ad98572d68d7aa3a
name <swlee1112@naver.com>
2023-09-11 17:54:14 +0900

    Create BOJ_10828.cs

---
+++ BOJ_10828.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace StackAndQueue
+{
+
+    class BOJ_10828
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse( Console.ReadLine());
+            Stack<int> S = new Stack<int>();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                string[] str = Console.ReadLine().Split(' ');
+                if (str[0] == "push")
+                    S.Push(int.Parse(str[1]));
+
+                else if(str[0] == "pop")
+                {
+                    if (S.Count == 0)
+                        sb.AppendLine("-1");
+                    else
+                    {
+                        sb.Append(S.Peek() + "\n");
+                        S.Pop();
+                    }
+                }
+
+                else if (str[0] == "size")
+                    sb.Append(S.Count + "\n");
+
+                else if (str[0] == "empty")
+                {
+                    if (S.Count == 0)
+                        sb.AppendLine("1");
+                    else
+                        sb.AppendLine("0");
+                }
+
+                else if (str[0] == "top")
+                {
+                    if (S.Count == 0)
+                        sb.AppendLine("-1");
+                    else
+                        sb.Append(S.Peek() + "\n");
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
