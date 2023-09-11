d28e86de3d3f2b2a99713fd58385b15e7336d777
name <swlee1112@naver.com>
2023-09-11 17:54:21 +0900

    Create BOJ_11399.cs

---
+++ BOJ_11399.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreedyAlgorithm
+{
+    
+    class BOJ_11399
+    {
+        static int n;
+        static int[] d;
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            List<int> intlist = new List<int>();
+            d = new int[n];
+
+            string[] inputTime = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+                intlist.Add(int.Parse(inputTime[i]));
+            intlist.Sort();
+
+            d[0] = intlist[0];
+            for (int i = 1; i < n; i++)
+                d[i] = d[i-1] + intlist[i];
+
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+                ans += d[i];
+            
+            Console.WriteLine(ans);
+        }
+    }
+    
+}
