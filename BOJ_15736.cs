5c0df391735a65e43696bf2c4650a3cfef7c991d
name <swlee1112@naver.com>
2023-09-12 18:30:21 +0900

    Create BOJ_15736.cs

---
+++ BOJ_15736.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MathAlgorithm
+{
+    
+    class BOJ_15736
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int idx = 1;
+            int count = 1;
+            int ans = 0;
+            while (idx <= n)
+            {
+                count += 2;
+                idx += count;
+                ans++;
+            }
+            Console.WriteLine(  ans);
+        }
+    }
+    
+}
