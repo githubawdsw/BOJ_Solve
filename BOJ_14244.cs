cb43d735014a6cee7b54a77cb207225932b85d18
name <swlee1112@naver.com>
2023-09-12 18:30:19 +0900

    Create BOJ_14244.cs

---
+++ BOJ_14244.cs
@@ -0,0 +1,18 @@
+﻿// BOJ_14244
+
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+string[] inputnm = Console.ReadLine().Split(' ');
+int n = int.Parse(inputnm[0]);
+int m = int.Parse(inputnm[1]);
+
+for (int i = 0; i < n - 1; i++)
+{
+    if (i < n - m)
+        sb.AppendLine($"{i} {i + 1}");
+    else
+        sb.AppendLine($"{n - m} {i + 1}");
+}
+Console.WriteLine(sb);
