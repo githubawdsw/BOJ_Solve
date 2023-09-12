a4f2395f2a42d6ef64a279be60e1e4f3d4d2278c
name <swlee1112@naver.com>
2023-09-12 18:30:17 +0900

    Create BOJ_11656.cs

---
+++ BOJ_11656.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting
+{
+    
+    class BOJ_11656
+    {
+        static string S;
+        static List<string> stringList = new List<string>();
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            S = Console.ReadLine();
+
+            stringList.Add(S);
+            for (int i = 1; i < S.Length; i++)
+            {
+                string suffix = S.Substring(i);
+                stringList.Add(suffix);
+            }
+            stringList = stringList.OrderBy(x => x).ToList();
+            for (int i = 0; i < stringList.Count; i++)
+            {
+                sb.AppendLine(stringList[i]);
+            }
+            Console.WriteLine(sb);
+        }
+    }
+    
+}
