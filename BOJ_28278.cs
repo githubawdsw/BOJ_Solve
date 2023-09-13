de92b6d5cb6f49f8955c5a139014e8edcdb2724d
name <swlee1112@naver.com>
2023-09-13 18:13:40 +0900

    Create BOJ_28278.cs

---
+++ BOJ_28278.cs
@@ -0,0 +1,44 @@
+﻿// BOJ_28278
+
+
+
+using System.Text;
+
+int n =int.Parse(Console.ReadLine());
+Stack<int> s = new Stack<int>();
+StringBuilder sb = new StringBuilder();
+while (n-- > 0)
+{
+    string[] input = Console.ReadLine().Split(' ');
+    if (input[0] == "1")
+        s.Push(int.Parse(input[1]));
+
+    else if (input[0] == "2")
+    {
+        if (s.Count > 0)
+            sb.AppendLine(s.Pop().ToString());
+        else
+            sb.AppendLine("-1");
+    }
+
+    else if (input[0] == "3")
+        sb.AppendLine(s.Count.ToString());
+
+    else if (input[0] == "4")
+    {
+        if (s.Count > 0)
+            sb.AppendLine("0");
+        else
+            sb.AppendLine("1");
+    }
+
+    else if (input[0] == "5")
+    {
+        if(s.Count > 0)
+            sb.AppendLine(s.Peek().ToString());
+        else 
+            sb.AppendLine("-1");
+    }
+}
+Console.WriteLine(  sb);
+
