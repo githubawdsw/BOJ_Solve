f20588cf67ed62429761a633baa584f0d6455b98
name <swlee1112@naver.com>
2023-09-13 18:13:33 +0900

    Create BOJ_1021.cs

---
+++ BOJ_1021.cs
@@ -0,0 +1,38 @@
+﻿// BOJ_1021
+  
+
+string[] inputnm = Console.ReadLine().Split(' ');
+int n = int.Parse(inputnm[0]);
+int m = int.Parse(inputnm[1]);
+string[] inputarr = Console.ReadLine().Split(' ');
+LinkedList<int> ll = new LinkedList<int>();
+for (int i = 1; i <= n; i++)
+    ll.AddLast(i);
+LinkedListNode<int> ln = ll.First;
+
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    int target = int.Parse(inputarr[i]);
+    int[] toarr = new int[55];
+    ll.CopyTo(toarr, 0);
+    int idx = Array.IndexOf(toarr, target);
+    while (target != ll.First.Value)
+    {
+        if(idx <= ll.Count / 2)
+        {
+            ll.AddLast(ll.First.Value);
+            ll.Remove(ll.First);
+            count++;
+        }
+        else
+        {
+            ll.AddFirst(ll.Last.Value);
+            ll.Remove(ll.Last);
+            count++;
+        }
+    }
+    ll.Remove(ll.First);
+}
+
+Console.WriteLine(count);
