b4c5408dd8fbd7335e224d41849c5bd9ab7b095c
name <swlee1112@naver.com>
2023-09-13 18:13:38 +0900

    Create BOJ_26069.cs

---
+++ BOJ_26069.cs
@@ -0,0 +1,19 @@
+﻿// BOJ_26069
+
+
+int n = int.Parse(Console.ReadLine());
+Dictionary<string, bool> dict = new Dictionary<string, bool>
+{
+    { "ChongChong", true }
+};
+
+for (int i = 0; i < n; i++)
+{
+    string[] inputrel = Console.ReadLine().Split(' ');
+    if (dict.ContainsKey(inputrel[1]) && dict[inputrel[1]] == true)
+        dict[inputrel[0]] = true;
+    else if (dict.ContainsKey(inputrel[0]) && dict[inputrel[0]] == true)
+        dict[inputrel[1]] = true;
+}
+Console.WriteLine(  dict.Count);
+
