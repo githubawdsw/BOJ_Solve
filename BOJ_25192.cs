1a1930370c2fbaf6e626dd0d79413c2c882657e6
name <swlee1112@naver.com>
2023-09-13 18:13:35 +0900

    Create BOJ_25192.cs

---
+++ BOJ_25192.cs
@@ -0,0 +1,20 @@
+﻿// BOJ_25192
+
+
+int n = int.Parse(Console.ReadLine());
+int count = 0;
+HashSet<string> hs = new HashSet<string>();
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+    if (input != "ENTER")
+        hs.Add(input);
+    else
+    {
+        count += hs.Count;
+        hs = new HashSet<string>();
+    }
+}
+count += hs.Count;
+Console.WriteLine(  count );
+
