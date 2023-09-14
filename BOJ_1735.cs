64bdd12287e26914442326039026ca7af542fea9
name <swlee1112@naver.com>
2023-09-14 16:08:21 +0900

    Create BOJ_1735.cs

---
+++ BOJ_1735.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace MathAlgorithm
+{
+    
+    class BOJ_1735
+    {
+        static void Main(string[] args)
+        {
+            string[] inputAB = Console.ReadLine().Split(' ');
+            int A = int.Parse(inputAB[0]);
+            int B = int.Parse(inputAB[1]);
+
+            string[] inputXY = Console.ReadLine().Split(' ');
+            int X = int.Parse(inputXY[0]);
+            int Y = int.Parse(inputXY[1]);
+
+            long LCM = B * Y / GCD(B, Y);
+            long numerator = (A * LCM / B) + (X * LCM / Y);
+
+            long ansGCD = GCD(LCM, numerator);
+            Console.WriteLine($"{numerator / ansGCD} {LCM / ansGCD}");
+        }
+
+        static long GCD(long a, long b)
+        {
+            if (b == 0)
+                return a;
+            return GCD(b, a % b);
+        }
+    }
+    
+}
