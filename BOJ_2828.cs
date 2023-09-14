3b65c2ed597351584c88e6bb372aa26cb3c12f77
name <swlee1112@naver.com>
2023-09-14 16:08:24 +0900

    Create BOJ_2828.cs

---
+++ BOJ_2828.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GreedyAlgorithm
+{
+    
+    class BOJ_2828
+    {
+        static void Main(string[] args)
+        {
+            string[] inputnm = Console.ReadLine().Split(' ');
+            int n = int.Parse(inputnm[0]);
+            int m = int.Parse(inputnm[1]);
+
+            int left = 1;
+            int right = m;
+            int ans = 0;
+
+            int j = int.Parse(Console.ReadLine());
+            for (int i = 0; i < j; i++)
+            {
+                int inputval = int.Parse(Console.ReadLine());
+                if (inputval <= right && inputval >= left)
+                    continue;
+                else if(inputval > right)
+                {
+                    int dis = inputval - right;
+                    left += dis;
+                    right += dis;
+                    ans += dis;
+                }
+                else if(inputval < left)
+                {
+                    int dis = left - inputval;
+                    left -= dis;
+                    right -= dis;
+                    ans += dis;
+                }
+            }
+
+            Console.WriteLine(  ans );
+        }
+    }
+    
+}
