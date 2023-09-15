baf8839dcb1908c903192ac32f0465b7f2869c6f
name <swlee1112@naver.com>
2023-09-15 16:12:02 +0900

    Create BOJ_1904.cs

---
+++ BOJ_1904.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Dynamic
+{
+    
+    class BOJ_1904
+    {
+        static int n;
+        static int[] d = new int[1000005];
+        static List<int> coin = new List<int>();
+        static void Main(string[] args)
+        {
+            string[] inputnk = Console.ReadLine().Split(' ');
+            n = int.Parse(inputnk[0]);
+            d[1] = 1;
+            d[2] = 2;
+            for (int i = 3; i <= n; i++)
+                d[i] = (d[i - 1] + d[i - 2]) % 15746;
+            Console.WriteLine(d[n]);
+        }
+    }
+    
+}
