6c11d482e662646b8a843e6e3bef274aaac44cb5
name <swlee1112@naver.com>
2023-09-15 16:11:58 +0900

    Create BOJ_13900.cs

---
+++ BOJ_13900.cs
@@ -0,0 +1,17 @@
+﻿// BOJ_13900
+
+
+int n = int.Parse(Console.ReadLine());
+string[] inputarr = Console.ReadLine().Split(' ');
+
+long ans = 0;
+long[] dp = new long[100005];
+
+for (int i = n - 1; i > 0; i--)
+	dp[i] = dp[i + 1] + long.Parse(inputarr[i]);
+
+for (int i = 1; i < n; i++)
+	ans += dp[i] * long.Parse(inputarr[i - 1]);
+
+
+Console.WriteLine(	ans);
