8f93157c94c8f7da2439694dd192926ba093eb4d
name <swlee1112@naver.com>
2023-09-15 16:12:05 +0900

    Create BOJ_1929.cs

---
+++ BOJ_1929.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+namespace MathAlgorithm
+{
+    
+    class BOJ_1929
+    {
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            string[] inputmn = Console.ReadLine().Split(' ');
+            int m = int.Parse(inputmn[0]);
+            int n = int.Parse(inputmn[1]);
+            bool[] check = new bool[10000005];
+
+            check[1] = true;
+            for (int i = 2; i *i  <= n; i++)
+            {
+                if (check[i] == true)
+                    continue;
+                for (int j = i; j * i <= n; j++)
+                    check[j * i] = true;
+            }
+            for (int i = m; i <= n; i++)
+                if (!check[i])
+                    sb.AppendLine(i.ToString());
+            Console.WriteLine(sb);
+        }
+    }
+    
+}
