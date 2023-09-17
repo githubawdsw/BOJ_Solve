9a0c224f4f7a172fff8ce8d5032107faa8dc2b7e
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2023-09-17 16:53:09 +0900

    [Silver III] Title: 이친수, Time: 64 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Dynamic
+{
+    
+    class BOJ_2193
+    {
+        static int N;
+        static long[] D = new long[ 93];
+        static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+
+            D[1] = 1;
+            D[2] = 1;
+            for (int i = 3; i <= N; i++)
+                D[i] = D[i - 1] + D[i -2] ;
+            Console.WriteLine(D[N]); 
+
+        }
+    }
+        
+}
