355ec1543ab12700c0f39a9e394fd26e004603a2
name <swlee1112@naver.com>
2023-09-18 16:10:26 +0900

    Create BOJ_3273.cs

---
+++ 백준/Silver/BOJ_3273.cs
@@ -0,0 +1,19 @@
+﻿// BOJ_3273
+    
+
+
+int n = int.Parse(Console.ReadLine());
+string[] inputarr = Console.ReadLine().Split(' ');
+int x = int.Parse(Console.ReadLine());
+
+int[] arr = new int[2000005];
+for (int i = 0; i < n; i++)
+    arr[int.Parse(inputarr[i])]++;
+
+int ans = 0;
+for (int i = 0; i < (x+1)/2; i++)
+{
+    if (arr[i] == 1 && arr[x - i] == 1)
+        ans++;
+}
+Console.WriteLine(ans);
