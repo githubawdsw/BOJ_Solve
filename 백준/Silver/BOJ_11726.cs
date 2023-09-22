1c7f7d1c3b023b91656ad11562ab4fe9f68ad0df
name <swlee1112@naver.com>
2023-09-22 16:34:24 +0900

    Create BOJ_11726.cs

---
+++ 백준/Silver/BOJ_11726.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Dynamic
+{
+    
+    class BOJ_11726
+    {
+        static int N;
+        static int[] D = new int[1001];
+
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+
+            D[1] = 1;
+            D[2] = 2;
+            for (int i = 3; i <= N; i++)
+            {
+                D[i] = (D[i - 1] + D[i - 2]) % 10007;
+            }
+            Console.WriteLine(D[N]);
+        }
+    }
+    
+}
