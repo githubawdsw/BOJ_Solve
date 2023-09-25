9a55271b5b0384848224a39db401d214e3fa6870
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2023-09-25 18:16:45 +0900

    [Silver III] Title: N과 M (6), Time: 68 ms, Memory: 5840 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Text;
+namespace Back_Tracking
+{
+    class N_And_M_6_15655
+    {
+        static int n, m;
+        static int[] arr = new int[10];
+        static bool[] isused = new bool[10];
+        static StringBuilder sb = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+            string[] inputvalue = Console.ReadLine().Split(" ");
+            n = int.Parse(inputvalue[0]);
+            m = int.Parse(inputvalue[1]);
+
+            string[] sequence = Console.ReadLine().Split(" ");
+            int[] sortValue = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                sortValue[i] = int.Parse(sequence[i]);
+            }
+            Array.Sort(sortValue);
+
+            reculsive( 0 , sortValue);
+            Console.WriteLine(sb);
+        }
+
+        public static void reculsive(int k , int[] sortValue)
+        {
+            if (k == m)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    sb.Append(sortValue[arr[i]] + " ");
+                }
+                sb.AppendLine();
+                return;
+            }
+
+            int start = 0;
+            if (k != 0)
+            {
+                start = arr[k - 1] + 1;
+            }
+            for (int i = start; i < n; i++)
+            {
+                if (!isused[i])
+                {
+                    isused[i] = true;
+                    arr[k] = i;
+                    reculsive(k+1, sortValue);
+                    isused[i] = false;
+                }
+            }
+        }
+    }
+}
