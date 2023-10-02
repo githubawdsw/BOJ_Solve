88e452c9fbc7c9502de4476deb1c1639556a08c4
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2023-10-02 18:17:48 +0900

    [Silver II] Title: 키로거, Time: 2540 ms, Memory: 387324 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baekjoon2
+{
+    
+    class BOJ_4949
+    {
+        
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            int T = int.Parse(Console.ReadLine());
+            while (T> 0)
+            {
+                T--;
+                string inputArr = Console.ReadLine();
+                LinkedList<string> ll = new LinkedList<string>();
+                for (int i = 0; i < inputArr.Length; i++)
+                    ll.AddLast(inputArr[i].ToString());
+                ll.AddLast("");
+
+                LinkedList<string> inputLL = new LinkedList<string>();
+                inputLL.AddLast(" ");
+                LinkedListNode<string> ln = inputLL.Last;
+                foreach (var one in ll)
+                {
+                    if (one == ">")
+                    {
+                        if (ln.Next == null)
+                            continue;
+                        else
+                            ln = ln.Next;
+                    }
+                    else if (one == "<")
+                    {
+                        if (ln.Previous == null)
+                            continue;
+                        else
+                            ln = ln.Previous;
+                    }
+                    else if (one == "-")
+                    {
+                        if (ln.Previous == null)
+                            continue;
+                        else
+                            inputLL.Remove(ln.Previous);
+                    }
+                    else
+                        inputLL.AddBefore(ln , one);
+                }
+                foreach (var one in inputLL)
+                    sb.Append(one);
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb);
+
+        }
+    }
+    
+}
