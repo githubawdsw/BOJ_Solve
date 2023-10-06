7a11fdde04ed1005a29d0736b1345950aa9ce984
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2023-10-06 15:50:13 +0900

    [Silver II] Title: N과 M (12), Time: 76 ms, Memory: 6964 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+namespace Back_Tracking
+{
+    class N_And_M_12_15666
+    {
+        static int n, m;
+        static int[] arr = new int[9];
+        static StringBuilder sb = new StringBuilder();
+        static int[] sortValue;
+        static void Main(string[] args)
+        {
+            string[] inputvalue = Console.ReadLine().Split(" ");
+            n = int.Parse(inputvalue[0]);
+            m = int.Parse(inputvalue[1]);
+
+            string[] sequence = Console.ReadLine().Split(" ");
+            sortValue = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                sortValue[i] = int.Parse(sequence[i]);
+            }
+
+            Array.Sort(sortValue, 0, n);
+
+            reculsive( 0 ,0 );
+            Console.Write(sb);
+        }
+        
+        
+        public static void reculsive(int index , int start )
+        {
+            if (index == m)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    sb.Append(arr[i] + " ");
+                }
+                sb.Append("\n");
+                return;
+            }
+
+            int temp = 0;
+            
+            for (int i = start; i < n; i++)
+            {
+                if (temp != sortValue[i])
+                {
+                    arr[index] = sortValue[i];
+                    temp = arr[index];
+                    reculsive(index + 1 , i );
+                }
+            }
+        }
+        
+    }
+}
