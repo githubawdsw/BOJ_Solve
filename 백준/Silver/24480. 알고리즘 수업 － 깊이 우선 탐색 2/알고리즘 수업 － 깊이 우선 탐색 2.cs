4f9eb1ba3b8e97290365b60eaaad0f6a82d43ef2
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2023-10-09 16:33:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 532 ms, Memory: 38884 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cs
@@ -0,0 +1,48 @@
+// BOJ_24480
+
+
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+int[] vis = new int[100005];
+List<int>[] arrList = new List<int>[100005];
+int count = 1;
+
+string[] inputnmr = Console.ReadLine().Split(' ');
+int n = int.Parse(inputnmr[0]);
+int m = int.Parse(inputnmr[1]);
+int r = int.Parse(inputnmr[2]);
+
+for (int i = 0; i < m; i++)
+{
+    string[] inputuv = Console.ReadLine().Split(" ");
+    int u = int.Parse(inputuv[0]);
+    int v = int.Parse(inputuv[1]);
+    if (arrList[u] == null)
+        arrList[u] = new List<int>();
+    arrList[u].Add(v);
+    if (arrList[v] == null)
+        arrList[v] = new List<int>();
+    arrList[v].Add(u);
+}
+for (int i = 0; i < arrList.Length; i++)
+    if (arrList[i] != null)
+        arrList[i] = arrList[i].OrderByDescending(x => x).ToList();
+
+dfs(r);
+for (int i = 1; i <= n; i++)
+    sb.AppendLine(vis[i].ToString());
+
+Console.WriteLine(sb);
+
+void dfs(int num)
+{
+    if (vis[num] == 0)
+        vis[num] = count++;
+    if (arrList[num] == null)
+        return;
+    foreach (var one in arrList[num])
+        if (vis[one] == 0)
+            dfs(one);
+}
