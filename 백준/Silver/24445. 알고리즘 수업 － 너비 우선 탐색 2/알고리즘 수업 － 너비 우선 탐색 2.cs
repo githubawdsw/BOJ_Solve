0bab33846e7dd77283ce27a1f08de1b4cbc81571
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2023-10-09 16:32:56 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 492 ms, Memory: 38316 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cs
@@ -0,0 +1,54 @@
+// BOJ_24445
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+
+string[] inputnmr = Console.ReadLine().Split(' ');
+int n = int.Parse(inputnmr[0]);
+int m = int.Parse(inputnmr[1]);
+int r = int.Parse(inputnmr[2]);
+
+List<int>[] arrList = new List<int>[100005];
+int count = 1;
+int[] vis = new int[100005];
+
+for (int i = 0; i < m; i++)
+{
+    string[] inputuv = Console.ReadLine().Split(' ');
+    int u = int.Parse(inputuv[0]);
+    int v = int.Parse(inputuv[1]);
+    if (arrList[u] == null)
+        arrList[u] = new List<int>();
+    arrList[u].Add(v);
+    if (arrList[v] == null)
+        arrList[v] = new List<int>();
+    arrList[v].Add(u);
+}
+for (int i = 0; i <= n; i++)
+    if (arrList[i] != null)
+        arrList[i] = arrList[i].OrderByDescending(x => x).ToList();
+
+Queue<int> q = new Queue<int>();
+q.Enqueue(r);
+vis[r] = count++;
+
+while (q.Count != 0)
+{
+    var cur = q.Dequeue();
+    if (arrList[cur] == null)
+        continue;
+
+    foreach (var one in arrList[cur])
+    {
+        if (vis[one] > 0)
+            continue;
+        vis[one] = count++;
+        q.Enqueue(one);
+    }
+}
+
+for (int i = 1; i <= n; i++)
+    sb.AppendLine(vis[i].ToString());
+
+Console.WriteLine(  sb);
