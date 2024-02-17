f0964eedb9cb76a8b60280f42436e7d306588d42
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2024-02-17 16:54:15 +0900

    [Silver I] Title: 두 박스, Time: 64 ms, Memory: 5520 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15973. 두 박스/두 박스.cs
+++ 백준/Silver/15973. 두 박스/두 박스.cs
@@ -1,10 +1,10 @@
 // BOJ_15973
 
 
-int[] box1 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int[] box2 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-(int, int, int, int) box1Point = (box1[0], box1[1], box1[2], box1[3]);
-(int, int, int, int) box2Point = (box2[0], box2[1], box2[2], box2[3]);
+long[] box1 = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+long[] box2 = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+(long, long, long, long) box1Point = (box1[0], box1[1], box1[2], box1[3]);
+(long, long, long, long) box2Point = (box2[0], box2[1], box2[2], box2[3]);
 
 if(box1Point.Item1 == box2Point.Item1 || box1Point.Item3 == box2Point.Item3)
 {
@@ -36,6 +36,8 @@ else if (box1Point.Item2 == box2Point.Item2 || box1Point.Item4 == box2Point.Item
         Console.WriteLine("NULL");
     else if (box1Point.Item3 < box2Point.Item1)
         Console.WriteLine("NULL");
+    else if (box1Point.Item3 == box2Point.Item1 || box1Point.Item1 == box2Point.Item3)
+        Console.WriteLine("LINE");
     else
         Console.WriteLine("FACE");
 }
@@ -52,22 +54,24 @@ else if (box1Point.Item2 == box2Point.Item4 || box1Point.Item4 == box2Point.Item
     else
         Console.WriteLine("LINE");
 }
-else if(FaceCheck(box2Point.Item1, box2Point.Item2) || FaceCheck(box2Point.Item1, box2Point.Item4)
-     || FaceCheck(box2Point.Item3, box2Point.Item2) || FaceCheck(box2Point.Item3, box2Point.Item4))
+else if(FaceCheck(box1Point, box2Point.Item1, box2Point.Item2) || FaceCheck(box1Point ,box2Point.Item1, box2Point.Item4)
+     || FaceCheck(box1Point, box2Point.Item3, box2Point.Item2) || FaceCheck(box1Point ,box2Point.Item3, box2Point.Item4)
+     || FaceCheck(box2Point, box1Point.Item1, box1Point.Item2) || FaceCheck(box2Point, box1Point.Item1, box1Point.Item4)
+     || FaceCheck(box2Point, box1Point.Item3, box1Point.Item2) || FaceCheck(box2Point, box1Point.Item3, box1Point.Item4))
         Console.WriteLine("FACE");
-else if(box1Point.Item1 < box2Point.Item1 && box1Point.Item3 > box2Point.Item3
-        && box1Point.Item2 > box2Point.Item2 && box1Point.Item4 < box2Point.Item4)
+else if(box1Point.Item1 <= box2Point.Item1 && box1Point.Item3 >= box2Point.Item3
+        && box1Point.Item2 >= box2Point.Item2 && box1Point.Item4 <= box2Point.Item4)
     Console.WriteLine("FACE");
-else if (box1Point.Item1 > box2Point.Item1 && box1Point.Item3 < box2Point.Item3
-        && box1Point.Item2 < box2Point.Item2 && box1Point.Item4 > box2Point.Item4)
+else if (box1Point.Item1 >= box2Point.Item1 && box1Point.Item3 <= box2Point.Item3
+        && box1Point.Item2 <= box2Point.Item2 && box1Point.Item4 >= box2Point.Item4)
     Console.WriteLine("FACE");
 else
     Console.WriteLine("NULL");
 
 
-bool FaceCheck(int x , int y)
+bool FaceCheck((long,long,long,long) compare, long x , long y)
 {
-    if (box1Point.Item1 < x && box1Point.Item3 > x && box1Point.Item2 < y && box1Point.Item4 > y)
+    if (compare.Item1 <= x && compare.Item3 >= x && compare.Item2 <= y && compare.Item4 >= y)
         return true;
     return false;
 }
