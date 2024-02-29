7fde01c3196d1d6a52ac47e280e52c2dfb8ce937
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2024-02-29 22:57:38 +0900

    [Gold V] Title: 꿀 따기, Time: 92 ms, Memory: 13888 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.cs
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.cs
@@ -2,49 +2,26 @@
 
 
 int n = int.Parse(Console.ReadLine());
-int[] inputHoney = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+long[] inputHoney = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 
-long[] leftstart = new long[100005];
-long[] rightstart = new long[100005];
+long[] dp = new long[100005];
 for (int i = 1; i <= n; i++)
 {
-    leftstart[i] = inputHoney[i - 1] + leftstart[i - 1];
-    rightstart[n - i + 1] = inputHoney[n - i] + rightstart[n - i + 1 + 1];
+    dp[i] = inputHoney[i - 1] + dp[i - 1];
 }
 
 long ans = 0;
-for (int i = 1; i <= n; i++)
+for (int i = 2; i < n; i++)
 {
-    for (int j = i + 1; j <= n; j++)
-    {
-        for (int k = 1; k <= n; k++)
-        {
-            if (k == i || k == j)
-                continue;
-
-            long sum = 0;
-
-            if (i < k)
-            {
-                sum += leftstart[k] - leftstart[i];
-                if (j < k)
-                    sum -= inputHoney[j - 1];
-            }
-            else
-                sum += rightstart[k] - rightstart[i];
-
-            if (j < k)
-                sum += leftstart[k] - leftstart[j];
-            else
-            {
-                sum += rightstart[k] - rightstart[j];
-                if (i > k)
-                    sum -= inputHoney[i - 1];
-            }
-
-            ans = Math.Max(sum, ans);
-        }
-    }
+    ans = Math.Max(ans, dp[n] - inputHoney[0] + dp[n] - inputHoney[i - 1] - dp[i]);
+}
+for (int i = 2; i < n; i++)
+{
+    ans = Math.Max(ans, dp[n] - inputHoney[n - 1] + dp[i - 1] - inputHoney[i - 1]);
+}
+for (int i = 2; i < n; i++)
+{
+    ans = Math.Max(ans, dp[i] - inputHoney[0] + dp[n] - dp[i - 1] - inputHoney[n - 1]);
 }
 
 Console.WriteLine(ans);
