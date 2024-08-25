27e7167238d5018d5a08b8e8754e313aa85c1fce
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2024-08-25 16:05:36 +0900

    [Silver I] Title: 계산 로봇, Time: 60 ms, Memory: 29824 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.cs
@@ -0,0 +1,36 @@
+// BOJ_22342
+
+
+int[] inputnm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int m = inputnm[0];
+int n = inputnm[1];
+
+int[,] board = new int[2005, 2005];
+int[,] dp = new int[2005, 2005];
+
+for (int i = 1; i < m; i++)
+{
+    string inputWeight = Console.ReadLine();
+    for (int j = 1; j < n; j++)
+	{
+        board[i, j] = inputWeight[j] - '0';
+	}
+}
+
+for (int j = 1; j <= n; j++)
+{
+    for (int i = 1; i <= m; i++)
+    {
+        dp[i, j] = Math.Max(dp[i, j], dp[i - 1, j - 1] + board[i, j]);
+        dp[i, j] = Math.Max(dp[i, j], dp[i, j - 1] + board[i, j]);
+        dp[i, j] = Math.Max(dp[i, j], dp[i + 1, j - 1] + board[i, j]);
+    }
+}
+
+int ans = 0;
+for (int i = 0; i < m; i++)
+{
+    ans = Math.Max(dp[i, n - 1], ans);
+}
+
+Console.WriteLine(ans);
