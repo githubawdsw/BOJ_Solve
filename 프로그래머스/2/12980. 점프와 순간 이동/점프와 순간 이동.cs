0b23dd668c8c6d211e80424e83c4e8ef56a02261
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2024-12-28 15:17:48 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.24 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        while(n != 0){
+            if(n % 2 == 0)
+                n /= 2;
+            else{
+                n--;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
