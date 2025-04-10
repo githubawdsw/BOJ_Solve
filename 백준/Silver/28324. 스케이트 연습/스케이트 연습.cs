18f5f69005e6a8d624e72c7fad82e3d7e5edb09f
githubawdsw <113886043+githubawdsw@users.noreply.github.com>
2025-04-10 16:57:20 +0900

    [Silver IV] Title: 스케이트 연습, Time: 56 ms, Memory: 6088 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cs
@@ -0,0 +1,20 @@
+// BOJ_28324
+
+
+int n = int.Parse(Console.ReadLine());
+int[] inputVelo = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int speed = 0;
+int ans = 0;
+
+for (int i = n - 1; i >= 0; i--)
+{
+    if (inputVelo[i] > speed)
+        speed++;
+    else
+        speed = inputVelo[i];
+
+    ans += speed;
+}
+
+Console.WriteLine(ans);
